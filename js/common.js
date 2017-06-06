8314507daf34ae4eb6307f37ba775bdbfb42a574
陈琪源 <289581763@qq.com>
2017-06-06 10:21:51 +0800

    知友app

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(function(){
+	$(window).resize(infinite);
+	function infinite() {
+		var htmlWidth = $('html').width();
+		if (htmlWidth >= 960) {
+			$("html").css({
+				"font-size" : "36px"
+			});
+		} else {
+			$("html").css({
+				"font-size" :  36 / 960  * htmlWidth + "px"
+			});
+		}
+	}infinite();
+});
